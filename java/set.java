a08c008078ca4bfd7048c4b3e3398ca6eb35773b
harshapink <harshavarthini297@gmail.com>
2019-10-25 11:05:27 +0530

    Update set.java

--- java/set.java
+++ java/set.java
@@ -1,4 +1,7 @@
-/*package whatever //do not write package name here */
+/* Given a set of numbers like <10, 36, 54,89,12> we want to find sum of weights based on the following conditions
+    1. 5 if a perfect square
+    2. 4 if multiple of 4 and divisible by 6
+    3. 3 if even numbe*/
 import java.util.*;
 import java.io.*;
 
