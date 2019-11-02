4e068b134369782e1abac76c59b2492b75eda78f
harshapink <harshavarthini297@gmail.com>
2019-11-02 12:49:54 +0000

    Update matrix.java

--- java/matrix.java
+++ java/matrix.java
@@ -3,58 +3,59 @@ THEY GAVE INPUT "WELCOMETOZOHOCORPORATION" AND HAVE TO  HAVE TO FIND THE SUBSTRI
 import java.util.*;
 import java.io.*;
 
-class GFG {
-   static int checksubstring(char a[][])
-    {
-        int s1=0,s2=0,e1=0,e2=0;
-        for(int i=0;i<5;i++)
+/*package whatever //do not write package name here */
+import java.util.*;
+import java.io.*;
+
+class D {
+	static int checklr(String s1,char a[][],int i,int j)
+	{
+     int t=0,k=0;
+     for(j=j;j<s1.length();j++)
+         {
+            if( a[i][j]==(s1.charAt(t)) && t<s1.length())
+			{
+                  t++;
+			      k=j;
+			}
+			else 
+				return 1;
+         }
+		 
+        if(t==s1.length())
 		{
-		    for(int j=0;j<5;j++)
-		    {
-		        if(a[i][j]=='t')
-		        {
-		            s1=i;
-		            s2=j;
-		            if(a[i][j+1]=='o' && a[i][j+2]=='o')
-		            {
-		                e1=i;
-		                e2=j+2;
-		                System.out.println("("+s1+","+s2+")"+"("+e1+","+e2+")");
-		                return 1;
-		            }
-		            
-		            else if( a[i][j-1]=='o' && a[i][j-2]=='o')
-		            {
-		                    e1=i;
-		                    e2=j-2;
-		                    System.out.println("("+s1+","+s2+")"+"("+e1+","+e2+")");
-		                    return 1;
-		            }
-		            else if(a[i+1][j]=='o' && a[i+2][j]=='o')
-		            {
-		                    e1=i+2;
-		                    e2=j;
-		                    System.out.println("("+s1+","+s2+")"+"("+e1+","+e2+")");
-		                    return 1;
-		            }
-		           else if(a[i-1][j]=='o' && a[i-2][j]=='o')
-		           {
-		                        e1=i-2;
-		                        e2=j;
-		                        System.out.println("("+s1+","+s2+")"+"("+e1+","+e2+")");
-		                       return 1;
-		                        
-		           }
-		           
-		        }
-		    }
-    }
-    return 0;
-    }
-	public static void main (String[] args) {
+			System.out.print(i+"  "+k);
+		}
+		return 1;
+         
+ }
+ static int  checktb(String s1,char a[][],int i,int j)
+ {
+	 int t=0,k=0;
+     for(i=i;i<s1.length();i++)
+         {
+            if( a[i][j]==(s1.charAt(t)) && t<s1.length())
+			{
+				//System.out.print(s1.charAt(i));
+                  t++;
+			      k=i;
+         }
+		 else
+			 return 1;
+		 }
+		 //System.out.print(t+" "+s1.length());
+        if(t==s1.length()-1)
+		{
+			System.out.print(k+" "+j);
+		}
+		return 1;
+	 
+ }
+		
+public static void main (String[] args) {
 		String s="welcometozohocorporation";
+		String s1="too";
 		int k=0;
-		int f=0;
 		char a[][]=new char[5][5];
 		for(int i=0;i<5;i++)
 		{
@@ -68,15 +69,18 @@ class GFG {
 		       
 		    }
 		}
-		f=checksubstring(a);
-		if(f==1)
-		{
-		    System.out.println("String found");
-		}
-		else{
-		    System.out.println("String not found");
-		}
-		
+		for(int i=0;i<5;i++){
+			
+           for(int j=0;j<5;j++)
+		   {
+			   
+     if(s1.charAt(0)==(a[i][j]))
+        {
+           checklr(s1,a,i,j);
+           checktb(s1,a,i,j);
+        }
 		
 	}
+}
+}
 }
