b74f18263445a6c25099fb09157e75ddb230ddff
harshapink <harshavarthini297@gmail.com>
2019-11-03 09:21:08 +0000

    Update reverse.java

--- java/set3/reverse.java
+++ java/set3/reverse.java
@@ -1,18 +1,50 @@
 /*package whatever //do not write package name here */
+/*package whatever //do not write package name here */
 import java.util.*;
 import java.io.*;
 
-class D {
+class GFG {
 public static void main (String[] args) {
 Scanner s=new Scanner(System.in);
 String s1=s.nextLine();
 String news="";
-String s2[]=s1.split(" ");                                                 
+int count=0; 
+/*String s2[]=s1.split(" ");                                                 
  for(int i=s2.length-1;i>=0;i--)
  {
 	 
 	 news=news+s2[i]+" ";
+ }*/
+ for(int i=0;i<s1.length();i++)
+ {
+     if(s1.charAt(i)==' ')
+     {
+         count++;
+     }
  }
- System.out.print(news);
+ String a[]=new String[count+1];
+ for(int i=0;i<s1.length();i++)
+ {
+     if(s1.charAt(i)!=' ')
+     {
+         news=news+s1.charAt(i);
+         if(i==s1.length()-1)
+         {
+             a[count]=news;
+         }
+        
+     }
+        else{
+            a[count]=news;
+            --count;
+            news="";
+        }
+        
+              
+ }
+ for(int i=0;i<a.length;i++)
+ {
+ System.out.print(a[i]+" ");
+ }
 }
 }
