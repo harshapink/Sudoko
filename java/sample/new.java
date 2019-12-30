8164ac78e81fa0af672591e02af6452d4ae86a31
harshapink <harshavarthini297@gmail.com>
2019-12-30 18:25:42 +0530

    Update new.java

--- java/sample/new.java
+++ java/sample/new.java
@@ -33,3 +33,40 @@ class GFG {
 		
 			}
 }
+
+
+
+
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    static void  check(char c)
+    {
+    HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+
+        if(map.containsKey(c))
+	{
+	    
+	    c=(char)((int)c+1);
+	    check(c);
+	  //  map.put(c,1);
+	}
+	else{
+	    map.put(c,1);
+	}
+	System.out.print(map);
+
+    }
+	public static void main (String[] args) {
+	    String s="aaabcdb";
+	    char c='\0';
+	    String s1="";
+	    for(int i=0;i<s.length();i++)
+	    {
+           check(s.charAt(i));
+	    }
+	
+		
+			}
+}
