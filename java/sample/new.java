7ef61dcd939be0befdd6d05c4531f1caf3c33491
harshapink <harshavarthini297@gmail.com>
2019-12-30 18:44:45 +0530

    Update new.java

--- java/sample/new.java
+++ java/sample/new.java
@@ -1,72 +1,34 @@
-/*package whatever //do not write package name here */
 
 import java.io.*;
 import java.util.*;
 class GFG {
-    static void check(char c)
+    static String   check(HashMap<Character,Integer> map,char c)
     {
-        
-
-    }
-	public static void main (String[] args) {
-	    String s="aaabcdb";
-	    char c='\0';
-	    String s1="";
-	    HashMap<Character,Integer> map=new HashMap<Character,Integer>();
-	    for(int i=0;i<s.length();i++)
-	    {
-	        	
-	if(map.containsKey(s.charAt(i)))
-	{
-	    
-	    c=(char)((int)s.charAt(i)+1);
-	    System.out.println(c);//check(c);
-	    map.put(c,1);
-	}
-	else{
-	    map.put(s.charAt(i),1);
-	}
-	
-	    }
-	    
-	
-		
-			}
-}
-
-
-
-
-
-import java.io.*;
-import java.util.*;
-class GFG {
-    static void  check(char c)
-    {
-    HashMap<Character,Integer> map=new HashMap<Character,Integer>();
-
+String s="";
         if(map.containsKey(c))
 	{
 	    
 	    c=(char)((int)c+1);
-	    check(c);
-	  //  map.put(c,1);
+	    check(map,c);
+	   //map.put(c,1);
 	}
 	else{
 	    map.put(c,1);
+	    s=s+c;
+	    System.out.print(s);
 	}
-	System.out.print(map);
-
+	return s;
     }
 	public static void main (String[] args) {
+	    HashMap<Character,Integer> map=new HashMap<Character,Integer>();
 	    String s="aaabcdb";
 	    char c='\0';
 	    String s1="";
 	    for(int i=0;i<s.length();i++)
 	    {
-           check(s.charAt(i));
+           s1=check(map,s.charAt(i));
 	    }
-	
-		
+//System.out.print(map);
+	   
 			}
 }
