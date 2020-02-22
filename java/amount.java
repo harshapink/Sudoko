a47a9088c4dccbaaa122e90e0748a0caf8d13359
harshapink <harshavarthini297@gmail.com>
2020-02-22 16:52:29 +0000

    Update amount.java

--- java/amount.java
+++ java/amount.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class A {   
+public class A {   
        static void word(char[] a)
        {
            int n=a.length;
@@ -10,7 +10,7 @@ String[] s = new String[]{ "zero", "one",
                                         "two", "three", "four", 
                                         "five", "six", "seven",  
                                             "eight", "nine"}; 
-String[] tl= new String[]{"", "ten", "eleven", "twelve",  
+String[] tl= new String[]{ "ten", "eleven", "twelve",  
                                         "thirteen", "fourteen", 
                                         "fifteen", "sixteen", "seventeen", 
                                         "eighteen", "nineteen"}; 
@@ -18,65 +18,96 @@ String[] te= new String[]{"", "", "twenty", "thirty", "forty",
                                             "fifty","sixty", "seventy",  
                                             "eighty", "ninety"}; 
 String[] tp= new String[] {"hundred", "thousand"}; 
-if(a.length==1 )
+
+String str ;
+
+int flag = 0;
+if(n>=5)
 {
-	System.out.print(s[a[t]-'0']);
-	return;
+
+    if(a[t]-'0'==1)
+        {
+            
+            System.out.print(" "+tl[a[t+1]-'0']);
+            flag =1;
+        }
+        else{
+            System.out.print(" "+te[a[t]-'0']+" ");
+        }
+    
+    str = new String(a); // 
+    str = str.substring(1); // 
+    a = str.toCharArray(); // 1000
 }
+if(n>=4)
+{
+    if( flag == 0 )
+    {
+        System.out.print(tl[a[t]-'0'] + "  Thousand ");
+    } 
+    else {
+        System.out.print(" Thousand ");
+    }
+
+    if(a[t+1] - '0' > 0 || a[t+2] - '0' >0 || a[t+3] - '0' >0  )
+    {
+        System.out.print(" and ");
+    }
+    
+    str = new String(a); // 
+    str = str.substring(1); // 
+    a = str.toCharArray(); //
+}
+
 if(n>=3)
 {
-	if(a[t]-'0' >0)
-	{
-		System.out.print(s[a[t]-'0']);
-	}
-	if(a[t+1]> 0 && a[t+2]>0 && a[t]-'0'>0)
-	{
-		System.out.print(tp[t]+"and");
-	}
-	else if(a[t]-'0' >0)
-	{
-		System.out.print(tp[a[t]-'0']);
-		
-	}
+
+    System.out.print(s[a[t]-'0'] + "  hundred "); // 
+
+    if(a[t+1] - '0' > 0 || a[t+2] - '0' >0 )
+    {
+        System.out.print(" and ");
+    }
+    
+    str = new String(a); // 
+    str = str.substring(1); // 
+    a = str.toCharArray(); // 
+    
 }
 if(n>=2)
 {
-	if(a[t+1]-'0' ==1 && a[t+2]-'0' >=0)
-	{
-		System.out.print(tl[a[t+1]-'0']);
-		return;
-	}
-	else if(a[t+1]-'0' ==0 && a[t+2]-'0' >0)
-	{
-		System.out.print(s[a[t+2]-'0']);
-		return;
-	}
-	else{
-		System.out.print(te[a[t+1]-'0']);
-	}
-		
+    if( a[t]- '0' > 0  )
+    {
+        if(a[t]-'0'==1)
+        {
+            System.out.print(" "+tl[a[t+1]-'0']);
+            return;
+        }
+        else{
+            System.out.print(" "+te[a[t]-'0']+" ");
+        }
+    }
+    
+    str = new String(a); // "23"
+    str = str.substring(1); // "3"
+    a = str.toCharArray(); // ['3']
+ 
+       
 }
 if(n>=1)
 {
-	if(a[t+2]-'0'>0)
-	{
-		System.out.print(s[a[t+2]-'0']);
-		return;
-	}
+    if(a[t]-'0'>0)
+    {
+        System.out.print(s[a[t]-'0']);
+    }
 }
 
-	System.out.print(s[0]);
+
 
 }
     
 public static void main (String[] args) {
-        word("005".toCharArray()); 
+
+        word((Integer.parseInt("1105")+"").toCharArray()); 
 }
 }
-
-
-
-
-
-
-
